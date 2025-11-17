2eec8821dc2d408eeee077b02a2d01c621a47a51
hud <baniczek@gmail.com>
2025-11-17 12:32:16 +0100

    asd

--- src/typescript/Layer/Backpropagation/BackpropagationFactory.ts
+++ src/typescript/Layer/Backpropagation/BackpropagationFactory.ts
@@ -9,29 +9,36 @@ import { BackpropagationToLSTM } from "./BackpropagationToLSTM";
 
 export class BackpropagationFactory {
   static create(previousLayer: Layers, layer: Layers): AbstractBackPropagation | null {
+    // --- Reguły oparte na typie bieżącej warstwy ---
     if (layer.getType() === LayerType.rnnlayer) {
       return new BackpropagationToRecurrent(layer, previousLayer);
     }
-
     if (layer.getType() === LayerType.lstm) {
       return new BackpropagationToLSTM(layer, previousLayer);
     }
-
-    if (previousLayer == null) {
-      if (layer.is1D()) {
-        return new Backpropagation1Dto1D(layer, previousLayer);
-      } else if (layer.getType() == LayerType.conv) {
-        return new Backpropagation3Dto1D(layer, previousLayer);
-      }
-    } else {
-      if (previousLayer.getType() == LayerType.maxpool) {
-        return new BackpropagationToMaxPool(layer, previousLayer);
-      } else if (previousLayer.getType() == LayerType.conv) {
-        return new BackpropagationToConv(layer, previousLayer);
-      } else if (previousLayer.is1D() || previousLayer.getType() == LayerType.fullyconnected) {
-        return new Backpropagation1Dto1D(layer, previousLayer);
-      }
+    if (layer.getType() === LayerType.maxpool) {
+      return new BackpropagationToMaxPool(layer, previousLayer);
     }
+    if (layer.getType() === LayerType.conv) {
+      return new BackpropagationToConv(layer, previousLayer);
+    }
+
+    // --- Reguły oparte na typie poprzedniej warstwy (jeśli istnieją) ---
+    if (previousLayer) {
+        if (previousLayer.is1D() || previousLayer.getType() == LayerType.fullyconnected) {
+            return new Backpropagation1Dto1D(layer, previousLayer);
+        }
+    } 
+    // --- Domyślne reguły ---
+    else {
+        if (layer.is1D()) {
+            return new Backpropagation1Dto1D(layer, previousLayer);
+        } else if (layer.is3D()) {
+            // Domyślna obsługa dla warstw 3D, jeśli żadna inna reguła nie pasuje
+            return new Backpropagation3Dto1D(layer, previousLayer);
+        }
+    }
+
     return null;
   }
 }
--- src/typescript/Layer/Backpropagation/BackpropagationToMaxPool.ts
+++ src/typescript/Layer/Backpropagation/BackpropagationToMaxPool.ts
@@ -1,62 +1,43 @@
 import { AbstractBackPropagation } from "./AbstractBackpropagation";
-import { Matrix } from "impulse-math-ts";
-import { Layers, Layers3D } from "../../types";
-import { getComputation } from "impulse-math-ts";
+import { CalcMatrix2D } from "impulse-math-device-ts";
+import { Layers } from "../../types";
+import { MaxPoolLayer } from "../MaxPool";
 
+/**
+ * Implementuje wsteczną propagację dla warstwy Max-Pooling.
+ * Działa jak "przełącznik", kierując gradienty tylko do tych neuronów,
+ * które miały maksymalną wartość podczas przechodzenia w przód.
+ */
 export class BackpropagationToMaxPool extends AbstractBackPropagation {
-  protected previousLayer: Layers3D | null = null;
+  propagate(
+    input: CalcMatrix2D,
+    numberOfExamples: number,
+    regularization: number,
+    layer: Layers,
+    sigma: CalcMatrix2D
+  ): CalcMatrix2D {
+    const maxPoolLayer = layer as MaxPoolLayer;
+    // @ts-ignore
+    const { switches, inputShape } = maxPoolLayer.cache;
+    const [inputHeight, inputWidth, inputDepth, batchSize] = inputShape;
 
-  propagate(input: Matrix, numberOfExamples: number, regularization: number, layer: Layers, sigma: Matrix): Matrix {
-    const prevLayer = this.previousLayer;
-    if (prevLayer) {
-      const result = getComputation().execute("fillZeros", new Matrix(prevLayer.Z.rows, prevLayer.Z.cols)) as Matrix;
+    // Stwórz macierz gradientu wejściowego wypełnioną zerami
+    const dX = new CalcMatrix2D(inputHeight * inputWidth * inputDepth, batchSize).setZeros();
+    const dXData = dX.get();
+    const sigmaData = sigma.get();
 
-      const filterSize = prevLayer.getFilterSize();
-      const stride = prevLayer.getStride();
-      const inputWidth = prevLayer.getWidth();
-      const inputHeight = prevLayer.getHeight();
-      const inputDepth = prevLayer.getDepth();
-      const outputWidth = prevLayer.getOutputWidth();
-      const outputHeight = prevLayer.getOutputHeight();
-      const outputDepth = prevLayer.getOutputDepth();
+    let switchIndex = 0;
+    const outputSize = sigma.get().length;
 
-      for (let m = 0; m < numberOfExamples; m++) {
-        for (let c = 0; c < outputDepth; c++) {
-          for (let h = 0; h < outputHeight; h++) {
-            for (let w = 0; w < outputWidth; w++) {
-              const vertStart = stride * h;
-              const vertEnd = vertStart + filterSize;
-              const horizStart = stride * w;
-              const horizEnd = horizStart + filterSize;
-
-              let _max = -Infinity;
-              const inputOffset = inputHeight * inputWidth * c;
-              const outputOffset = outputHeight * outputWidth * c;
-              let maxX = 0;
-              let maxY = 0;
-
-              for (let y = 0, vStart = vertStart; y < filterSize; y++, vStart++) {
-                for (let x = 0, hStart = horizStart; x < filterSize; x++, hStart++) {
-                  if (prevLayer.Z.data && _max < prevLayer.Z.data[inputOffset + vStart * inputWidth + hStart][m]) {
-                    _max = prevLayer.Z.data[inputOffset + vStart * inputWidth + hStart][m];
-                    maxX = hStart;
-                    maxY = vStart;
-                  }
-                }
-              }
-
-              if (result.data && sigma.data) {
-                result.data[inputOffset + maxY * inputWidth + maxX][m] =
-                  sigma.data[outputOffset + h * outputWidth + w][m];
-              }
-            }
-          }
-        }
-      }
-
-      return result;
+    // Rozprowadź gradienty do odpowiednich miejsc
+    for (let i = 0; i < outputSize; i++) {
+        const maxIndex = switches[switchIndex++];
+        dXData[maxIndex] += sigmaData[i];
     }
 
-    return new Matrix();
+    dX.set(dXData);
+
+    // Przekształć gradient do kształtu oczekiwanego przez poprzednią warstwę
+    return dX.reshape(inputHeight * inputWidth * inputDepth, batchSize);
   }
 }
